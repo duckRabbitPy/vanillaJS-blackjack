41d2f8704ae8b3898e6307d6990ae84903caf686
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-01 21:36:44 +0000

    Revert "add serverless function"

--- public/api/database.js
+++
@@ -1,5 +0,0 @@
-export default function handler(request, response) {
-  const { name } = request.query;
-  const db_json = { fhohdsfnajsonf: { score: 5545, username: name } };
-  const response = response.json(200).send(db_json);
-}
